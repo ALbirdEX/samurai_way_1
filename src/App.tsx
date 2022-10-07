0021bc34b653918889e7e8e39f3a3c0e5a72a5cf
ALbirdEX <alex123890@gmail.com>
2022-10-07 13:09:43 +0300

    Add Footer.tsx
    handed over Footer.ts to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
-import Technologies from "./Technologies";
+import {Technologies} from "./Technologies";
+import Footer from "./Footer";
 
 export const App = () => {
     return (<div>
-            {/*<div><Header/></div>*/}
-            <div><Technologies/></div>
-        </div>);
+        <div><Header/></div>
+        <div><Technologies/></div>
+        <div><Footer/></div>
+    </div>);
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            it-Kamasutra.com
+        </div>
+    );
+};
+
+export default Footer;
