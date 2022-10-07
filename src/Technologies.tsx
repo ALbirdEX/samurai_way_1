fc929328e270d6d35cccbbf2a560215de7786a20
ALbirdEX <alex123890@gmail.com>
2022-10-07 12:09:46 +0300

    Exploring the Components

---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="/">CSS</a></li>
+                <li><a href="/">JS</a></li>
+                <li><a href="/">TS</a></li>
+                <li><a href="/">REACT</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
