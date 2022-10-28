97e0f7e54ee4e890fd82b0a9318860a909e8d02c
ALbirdEX <alex123890@gmail.com>
2022-10-28 22:39:05 +0300

    route, browser-router, маршрутизация

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import './App.css'
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+import {Route, Routes} from "react-router-dom";
+import {New} from "./components/New/New";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 export function App() {
     return (
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className="app-wrapper-content">
+                <Routes>
+                    <Route path={"/profile"} element={<Profile/>}/>
+                    <Route path={"/dialog"} element={<Dialogs/>}/>
+                    <Route path={"/new"} element={<New/>}/>
+                    <Route path={"/music"} element={<Music/>}/>
+                    <Route path={"/settings"} element={<Settings/>}/>
+                </Routes>
+            </div>
         </div>
     );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import classes from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export function Navbar() {
     return (
         <nav className={classes.menu}>
-            <div className={classes.item}><a>Profile</a></div>
-            <div className={`${classes.item} ${classes.active}`}><a>Message</a></div>
-            <div className={classes.item}><a>News</a></div>
-            <div className={classes.item}><a>Music</a></div>
-            <div className={classes.item}><a>Setting</a></div>
+            <div className={classes.item}><a><NavLink to={"/profile"}>Profile</NavLink></a></div>
+            <div className={`${classes.item} ${classes.active}`}><a><NavLink to={"/dialog"}>Message</NavLink></a></div>
+            <div className={classes.item}><a><NavLink to={"/new"}>New</NavLink></a></div>
+            <div className={classes.item}><a><NavLink to={"/music"}>Music</NavLink></a></div>
+            <div className={classes.item}><a><NavLink to={"/settings"}>Settings</NavLink></a></div>
         </nav>
     );
 }
