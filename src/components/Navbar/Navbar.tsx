8989a06e4276f24dca910a030936aa43d97c4f2e
ALbirdEX <alex123890@gmail.com>
2022-10-28 22:42:12 +0300

    route, browser-router, маршрутизация
    delet <a></a> Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ import {NavLink} from "react-router-dom";
 export function Navbar() {
     return (
         <nav className={classes.menu}>
-            <div className={classes.item}><a><NavLink to={"/profile"}>Profile</NavLink></a></div>
-            <div className={`${classes.item} ${classes.active}`}><a><NavLink to={"/dialog"}>Message</NavLink></a></div>
-            <div className={classes.item}><a><NavLink to={"/new"}>New</NavLink></a></div>
-            <div className={classes.item}><a><NavLink to={"/music"}>Music</NavLink></a></div>
-            <div className={classes.item}><a><NavLink to={"/settings"}>Settings</NavLink></a></div>
+            <div className={classes.item}><NavLink to={"/profile"}>Profile</NavLink></div>
+            <div className={`${classes.item} ${classes.active}`}><NavLink to={"/dialog"}>Message</NavLink></div>
+            <div className={classes.item}><NavLink to={"/new"}>New</NavLink></div>
+            <div className={classes.item}><NavLink to={"/music"}>Music</NavLink></div>
+            <div className={classes.item}><NavLink to={"/settings"}>Settings</NavLink></div>
         </nav>
     );
 }
