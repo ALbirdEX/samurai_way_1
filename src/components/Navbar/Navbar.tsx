6abc21d514d589b2bd739f1e6100dcddf028b791
ALbirdEX <alex123890@gmail.com>
2022-10-29 12:50:13 +0300

    route, browser-router, маршрутизация
    Navbar.tsx NavLink,  className={({isActive}) => isActive ? classes.activeLink : classes.noActive}

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,32 @@ import {NavLink} from "react-router-dom";
 export function Navbar() {
     return (
         <nav className={classes.menu}>
-            <div className={classes.item}><NavLink to={"/profile"}>Profile</NavLink></div>
-            <div className={`${classes.item} ${classes.active}`}><NavLink to={"/dialog"}>Message</NavLink></div>
-            <div className={classes.item}><NavLink to={"/new"}>New</NavLink></div>
-            <div className={classes.item}><NavLink to={"/music"}>Music</NavLink></div>
-            <div className={classes.item}><NavLink to={"/settings"}>Settings</NavLink></div>
+            <div className={classes.item}>
+                <NavLink to={"/profile"}
+                         className={({isActive}) => isActive ? classes.activeLink : classes.noActive}>Profile</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={"/dialog"}
+                         className={({isActive}) => isActive ? classes.activeLink : classes.noActive}>Message</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={"/new"}
+                         className={({isActive}) => isActive ? classes.activeLink : classes.noActive}>New</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={"/music"}
+                         className={({isActive}) => isActive ? classes.activeLink : classes.noActive}>Music</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={"/settings"} className={({isActive})=>isActive ? classes.activeLink : classes.noActive}>Settings</NavLink>
+            </div>
         </nav>
+        /* <nav className={classes.menu}>
+             <div className={classes.item || classes.active}><a href={"/profile"}>Profile</a></div>
+             <div className={`${classes.item} ${classes.active}`}><a href={"/dialog"}>Message</a></div>
+             <div className={classes.item}><a href={"/new"}>New</a></div>
+             <div className={classes.item}><a href={"/music"}>Music</a></div>
+             <div className={classes.item}><a href={"/settings"}>Settings</a></div>
+          </nav>*/
     );
 }
